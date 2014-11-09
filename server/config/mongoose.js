3141a38767075627e0917e6f2e42b7d962d42db6
Toki-webdesign <sandor.tokodi@gmail.com>
2014-11-09 15:13:23 +0100

    Authentication

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+ crypto = require('crypto');
 
 module.exports = function(config) {
     mongoose.connect(config.db);
@@ -11,19 +12,46 @@ module.exports = function(config) {
     var userSchema = mongoose.Schema({
         firstName: String,
         lastName: String,
-        userName: String
+        userName: String,
+        salt: String,
+        hashed_pwd: String,
+        roles: [String]
     });
+
+    userSchema.methods = {
+        authenticate: function(passwordToMatch) {
+            return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+        }
+    };
     var User = mongoose.model('User', userSchema);
 
-    User.find({}).exec(function(err, collection){
-        if(collection.length === 0){
-            User.create({firstName:'Sanyi',lastName:'Tokodi', userName:'Toki'});
-            User.create({firstName:'Anikó',lastName:'Tokodi', userName:'Saci'});
-            User.create({firstName:'Anna',lastName:'Tokodi', userName:'Anna'});
-            User.create({firstName:'Gergő',lastName:'Tokodi', userName:'Gergő'});
+    User.find({}).exec(function(err, collection) {
+        if(collection.length === 0) {
+            var salt, hash;
+            salt = createSalt();
+            hash = hashPwd(salt, 'Sanyi');
+            User.create({firstName:'Sanyi',lastName:'Tokodi', userName:'Toki', salt: salt, hashed_pwd: hash, roles: ['admin']});
+            salt = createSalt();
+            hash = hashPwd(salt, 'Anikó');
+            User.create({firstName:'Anikó',lastName:'Tokodi', userName:'Saci', salt: salt, hashed_pwd: hash, roles: []});
+            salt = createSalt();
+            hash = hashPwd(salt, 'Anna');
+            User.create({firstName:'Anna',lastName:'Tokodi', userName:'Anna', salt: salt, hashed_pwd: hash});
+            salt = createSalt();
+            hash = hashPwd(salt, 'Gergő');
+           User.create({firstName:'Gergő',lastName:'Tokodi', userName:'Gergő', salt: salt, hashed_pwd: hash});
         }
 
     })
 
 
+}
+
+function createSalt() {
+    return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+    var hmac = crypto.createHmac('sha1', salt);
+    return hmac.update(pwd).digest('hex');
 }
